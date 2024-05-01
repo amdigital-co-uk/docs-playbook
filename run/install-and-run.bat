5319df086c81a571f786e1545df952c44f904a6b
Sam Bayliss <bayliss.sam@hotmail.co.uk>
2024-05-01 13:35:40 +0100

    add how section

--- run/install-and-run.bat
+++ run/install-and-run.bat
@@ -1,7 +1,11 @@
 @echo off
 py -m pip install mkdocs-material --upgrade
-py -m pip install mkdocs-awesome-pages-plugin
-py -m pip install mkdocs-glightbox
+py -m pip install mkdocs-awesome-pages-plugin --upgrade
+py -m pip install mkdocs-glightbox --upgrade
+py -m pip install mkdocs-search --upgrade
+py -m pip install mkdocs-tags --upgrade
+py -m pip install mkdocs-meta --upgrade
+
 py -m mkdocs serve
 exit
 
