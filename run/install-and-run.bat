66a498ebfc35438f5492f216e9adf05d3267ab71
Sam Bayliss <bayliss.sam@hotmail.co.uk>
2024-05-03 09:56:36 +0100

    remove changes to run script

--- run/install-and-run.bat
+++ run/install-and-run.bat
@@ -1,7 +1,7 @@
 @echo off
 py -m pip install mkdocs-material --upgrade
-py -m pip install mkdocs-awesome-pages-plugin --upgrade
-py -m pip install mkdocs-glightbox --upgrade
+py -m pip install mkdocs-awesome-pages-plugin
+py -m pip install mkdocs-glightbox
 py -m mkdocs serve
 exit
 
